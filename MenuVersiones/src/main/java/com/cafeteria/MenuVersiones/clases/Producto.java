5a49f121a8967edc066bb0e4323a43be52323192
maytokidd <sotomayorsb@gmail.com>
2025-09-20 02:55:15 -0500

    feat: agregar clase Producto

---
+++ MenuVersiones/src/main/java/com/cafeteria/MenuVersiones/clases/Producto.java
@@ -0,0 +1,28 @@
+package com.cafeteria.MenuVersiones.clases;
+
+public class Producto {
+    private String nombre;
+    private double precio;
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Producto(String nombre, double precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+}
+
